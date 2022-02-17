aa43b56327145149b8132fa89494c95140a19284
Andrew <ae.brown@mac.com>
2022-02-17 10:55:53 +0000

    completed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UnSplash_Downloader
 Downloading images from UnSplash in Python
+
+This did not require as much code as I thought it would... But useful none the less
+
+All credit to this medium post: https://medium.com/analytics-vidhya/%EF%B8%8F-do-some-automate-work-with-python-a4c5d50da79c
--- src/photo_downloader.py
+++ src/photo_downloader.py
@@ -1,8 +1,34 @@
-import requests 
+import requests
 from bs4 import BeautifulSoup
+import os
 import lxml
+from tqdm import tqdm
 
-def photo_download(url):
-    requests = requests.get(url, allow_redirects=True)
-	data = BeautifulSoup(request.content,'lxml')
+output_path = '/Users/andrewbrown/images'
 
+def photo_downloader(url, out_dir):
+    request = requests.get(url,allow_redirects = True)
+    data = BeautifulSoup(request.text,'lxml')
+    all_image=data.find_all('figure',itemprop="image")
+    count =0
+    for i in tqdm(all_image):
+        url=i.find('a',rel="nofollow")
+        if url != None:
+            i_url = url['href']
+            photo_bytes = requests.get(i_url,allow_redirects=True)
+            with open(os.path.join(out_dir,f'{count}3d.jpg'),'wb') as photo:
+                photo.write(photo_bytes.content)
+                count +=1
+
+    print("Done")
+
+
+
+if __name__ == "__main__":
+
+    url_name = "https://unsplash.com/s/photos/3d"
+    dir_name = url_name.split('/')[-1]
+    if not os.path.isdir(os.path.join(output_path,dir_name)):
+        os.mkdir(os.path.join(output_path,dir_name))
+
+    photo_downloader(url_name, os.path.join(output_path,dir_name))
